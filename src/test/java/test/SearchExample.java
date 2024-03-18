b9f56002f5b19ca6d0a3d8faa31c5327615a0b44
Alina Pestova <ali.pestova@yandex.ru>
2024-03-18 22:40:50 +0300

    added Test

--- src/test/java/test/SearchExample.java
+++ src/test/java/test/SearchExample.java
@@ -15,8 +15,6 @@ public class SearchExample {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000;
     }
     @Test
     void exampleCodeOfJunit5(){
